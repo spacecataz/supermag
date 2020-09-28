d780422c23141b6ddd5b62696a0b9ca978333376
Dan Welling <daniel.welling@uta.edu>
2020-09-28 09:14:39 -0500

    New files for formal unit testing of this package.

---
+++ test_supermag.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+'''
+
+'''
+
+import numpy as np
+import datetime as dt
+import unittest
+
+import supermag
+
+# Define test case classes to group related tests together:
+class TestReadImf(unittest.TestCase):
+    pass
+
+if __name__=='__main__':
+    unittest.main()
